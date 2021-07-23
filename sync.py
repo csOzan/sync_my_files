6c138d7dd6956a3381e0a692bc971402d99ba784
Ozan Çakmak <56032671+spaceSpaghetty@users.noreply.github.com>
2021-07-24 00:31:53 +0300

    Update sync.py
    I refactored sync module since old one was part of legacy code

--- sync.py
+++ sync.py
@@ -1,42 +1,44 @@
+from dirsync import sync
 # sync module is for syncing functions
-def documents_(bus_letter, stream):
-    source = "C:\\Users\\{}\\OneDrive\\Documents".format(_userName) #NOTE define path without Onedrive then modularize it
-    # to sync documents, mapped to user\documents
-    # if else structure to find upload download
-    source = 'C:\\Users\\spaceSpaghetty\\Documents'
-    target = "{}:\\Documents".format(bus_letter)
-    if(stream == 0):
-        # upload
-        print('Uploading...')
+def documents(sync_token, source,target):
+    if(sync_token == 1):
+        print('Uploading new files to drive...')
         sync(source,target,'sync')
         print('Upload complete ')
-    elif(stream == 1):# download
-        print('Downloading...')
+    elif(sync_token == 2):
+        print('Downloading missing files from drive...')
         sync(target, source,'sync')
         print('Download Complete')
-    else:# complete sync
-        print('Syncing...')    # to sync pictures, mapped to user\pictures
+    else:
+        print('Syncing Documents...')
         sync(target, source,'sync')
         sync(source, target,'sync')
         print('Syncing Complete')
-def pictures(bus_letter,stream):
-    source = "C:\\Users\\{}\\OneDrive\\Pictures".format(_userName)
-    target = "{}:\\Pictures".format(bus_letter)
-    if(stream == 0):
-        # upload
-        print('Uploading...')
+def pictures(sync_token, source, target):
+    if(sync_token == 1):
+        print('Uploading new files to drive...')
         sync(source,target,'sync')
         print('Upload complete ')
-    elif(stream == 1):# download
-        print('Downloading...')
+    elif(sync_token == 2):
+        print('Downloading missing files from drive...')
         sync(target, source,'sync')
         print('Download Complete')
-    else:# complete sync
-        print('Syncing...')    # to sync pictures, mapped to user\pictures
+    else:
+        print('Syncing Pictures...')
         sync(target, source,'sync')
         sync(source, target,'sync')
         print('Syncing Complete')
-    # to sync a main folder which is like main drive path, Onedrive or google drive root.
-    # it also includes a seperate documents folder, a pictures f
-def home(bus_letter,stream):
-    pass
+def home(sync_token, source, target):
+    if(sync_token == 1):
+        print('Uploading new files to drive...')
+        sync(source,target,'sync')
+        print('Upload complete ')
+    elif(sync_token == 2):
+        print('Downloading missing files from drive...')
+        sync(target, source,'sync')
+        print('Download Complete')
+    else:
+        print('Syncing home directory...')
+        sync(target, source,'sync')
+        sync(source, target,'sync')
+        print('Syncing Complete')
