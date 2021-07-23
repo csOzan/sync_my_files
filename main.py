a317984de1d508115e6f878112df5eee468394d6
Ozan Çakmak <56032671+spaceSpaghetty@users.noreply.github.com>
2021-07-24 00:54:45 +0300

    Update main.py
    Created a user interface. Still not ready for first run

--- main.py
+++ main.py
@@ -11,13 +11,24 @@ def main():
         paths = tools.initialize()
     else:
         paths = tools.reader()
-    
-    sync_token = int(input("For Upload Only Enter '1'\nFor Download Only Enter '2'\nFor Complete Sync Enter '3'"))
-    if (sync_token == 1):
-        sync.documents
-    elif (sync_token == 2):
-        pass
-    elif (sync_token == 3):
-        pass
-    else:
-        print('You have entered wrong number aborting...')
+    while True:
+        # move termination condition warning to first prompt 
+        sync_token = int(input("For Upload Only Enter '1'\nFor Download Only Enter '2'\nFor Complete Sync Enter '3'"))
+        if (sync_token == 0):
+            exit() 
+        elif (sync_token == 1 or sync_token == 2 or sync_token == 3):
+            break
+        else:
+            print("You have entered wrong number(for exit enter 0)")
+    while True:
+        which_to_sync = int(input("Enter:\n1:Documents\t2:Pictures\t3:home\t4:all"))
+        if(which_to_sync == 0):
+            exit()
+        elif(which_to_sync == 1 or which_to_sync == 2 or which_to_sync == 3 or which_to_sync == 4):
+            if(which_to_sync == 1):# wait to fix source-target paths
+                sync.documents(sync_token,paths["documents"],)
+
+        else:
+            print('You have entered wrong number,(for exit enter 0)')
+            continue
+        print('Restarting sync engine, press 0 for exit when prompt asked')
