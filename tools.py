0baecb179fc0b0d75cca4286c811a96b92023824
Ozan Çakmak <56032671+spaceSpaghetty@users.noreply.github.com>
2021-07-23 23:28:34 +0300

    I created a first time initalization user inerface

--- tools.py
+++ tools.py
@@ -4,4 +4,26 @@ import os
 
 def first_write():
     #NOTE check for directory existence
-    document_
+    while True:
+        document_path = input('Enter your documents folder path: ')
+        if(os.path.isdir(document_path) == False):
+            print('The path you entered is not available')
+            continue
+        pictures_path = input('Enter your pictures folder path: ')
+        if(os.path.isdir(pictures_path) == False):
+            print('The path you entered is not available')
+            continue
+        home_path = input('Enter your home folder path')
+        if(os.path.isdir(home_path) == False):
+            print('The path you entered is not available')
+            continue
+        paths = {
+            "home": home_path,
+            "documents": document_path,
+            "pictures" : pictures_path
+        }
+
+        with open("settings.json","w") as fd:
+            json.dump(paths, fd)
+        
+
